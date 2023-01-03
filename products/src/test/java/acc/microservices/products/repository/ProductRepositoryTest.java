e54def8291d0f285ffce618835a723b6b898c6d5
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-01-03 15:39:05 -0500

    Products: Repo Test

--- products/src/test/java/acc/microservices/products/repository/ProductRepositoryTest.java
+++ products/src/test/java/acc/microservices/products/repository/ProductRepositoryTest.java
@@ -1,6 +1,5 @@
 package acc.microservices.products.repository;
 
-import java.util.Date;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
@@ -18,18 +17,8 @@ public class ProductRepositoryTest {
 
   @Test
   public void whenSearchByCategoryThenReturnProductsList() {
-    Product p1 = Product.builder()
-        .category(Category.builder().id(1L).build())
-        .name("Computer")
-        .description("")
-        .stock(15)
-        .createdAt(new Date())
-        .price(1225.5)
-        .status("Created")
-        .build();
-    this.productRepository.save(p1);
 
-    List<Product> listProducts = this.productRepository.findByCategory(p1.getCategory());
-    Assertions.assertThat(listProducts.size()).isEqualTo(3);
+    List<Product> listProducts = this.productRepository.findByCategory(Category.builder().id(1L).build());
+    Assertions.assertThat(listProducts.size()).isEqualTo(2);
   }
 }
