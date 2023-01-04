d6b77af512c9c7cd362798196df003bb866a5093
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-01-04 15:14:04 -0500

    Shopping: Initial state

---
+++ shopping/src/test/java/acc/microservices/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.microservices.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
