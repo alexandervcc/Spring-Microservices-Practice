da2c86c5beb6719fbb5e5478fb9f44ccfd0775fa
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-01-09 13:04:20 -0500

    Admin: Initial Status

---
+++ admin-service/src/test/java/acc/microservices/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.microservices.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
