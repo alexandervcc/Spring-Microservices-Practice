abc960e6bf4b7b7eeeb3c33cf159ff72f39fac1a
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-01-09 11:10:59 -0500

    Shopping: Feign + Resilience Fallback

--- shopping/src/main/java/acc/microservices/shopping/client/CustomerFallbackConfig.java
+++ shopping/src/main/java/acc/microservices/shopping/client/CustomerFallbackConfig.java
@@ -1,28 +1,19 @@
 package acc.microservices.shopping.client;
 
-import java.time.Duration;
 
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import acc.microservices.shopping.model.pojos.Customer;
 
 @Configuration
-public class CustomerFallbackConfig {
+public class CustomerFallbackConfig implements CustomerClient {
 
-  @Bean
-  public Customizer<Resilience4JCircuitBreakerFactory> circuitBreakerFactoryCustomizer() {
-    CircuitBreakerConfig cbConfig = CircuitBreakerConfig.custom()
-        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
-        .slidingWindowSize(5)
-        .failureRateThreshold(20.0f)
-        .waitDurationInOpenState(Duration.ofSeconds(5))
-        .permittedNumberOfCallsInHalfOpenState(5)
-        .build();
-    return resilience4JCircuitBreakerFactory -> resilience4JCircuitBreakerFactory
-        .configure(builder -> builder.circuitBreakerConfig(cbConfig), "CustomerClient#getCustomer(id)");
+	@Override
+	public ResponseEntity<Customer> getCustomer(long id) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(Customer.builder().id(0L).firstName("Default User").build());
+	}
 
-  }
 }
--- shopping/src/main/java/acc/microservices/shopping/services/InvoiceServiceImpl.java
+++ shopping/src/main/java/acc/microservices/shopping/services/InvoiceServiceImpl.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 
 import acc.microservices.shopping.client.CustomerClient;
 import acc.microservices.shopping.client.ProductClient;
@@ -17,71 +18,72 @@ import acc.microservices.shopping.model.pojos.Product;
 import acc.microservices.shopping.repository.InvoiceItemRepository;
 import acc.microservices.shopping.repository.InvoiceRepository;
 import acc.microservices.shopping.services.interfaces.InvoiceService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class InvoiceServiceImpl implements InvoiceService {
-  private final InvoiceRepository invoiceRepository;
-  private final InvoiceItemRepository invoiceItemRepository;
-  private final CustomerClient customerClient;
-  private final ProductClient productClient;
+	private final InvoiceRepository invoiceRepository;
+	private final InvoiceItemRepository invoiceItemRepository;
+	private final CustomerClient customerClient;
+	private final ProductClient productClient;
 
-  @Override
-  public List<Invoice> findInvoiceAll() {
-    return invoiceRepository.findAll();
-  }
+	@Override
+	public List<Invoice> findInvoiceAll() {
+		return invoiceRepository.findAll();
+	}
 
-  @Override
-  public Invoice createInvoice(Invoice invoice) {
-    Optional<Invoice> invoiceDB = invoiceRepository.findByNumberInvoice(invoice.getNumberInvoice());
-    if (invoiceDB.isPresent()) {
-      throw new NotFoundException("Invoice with given ID already exists.");
-    }
-    invoice.setState("CREATED");
-    invoice = this.invoiceRepository.save(invoice);
+	@Override
+	public Invoice createInvoice(Invoice invoice) {
+		Optional<Invoice> invoiceDB = invoiceRepository.findByNumberInvoice(invoice.getNumberInvoice());
+		if (invoiceDB.isPresent()) {
+			throw new NotFoundException("Invoice with given ID already exists.");
+		}
+		invoice.setState("CREATED");
+		invoice = this.invoiceRepository.save(invoice);
 
-    invoice.getItems().forEach(item -> {
-      this.productClient.updateProductStock(item.getId(), item.getQuantity());
-    });
+		invoice.getItems().forEach(item -> {
+			this.productClient.updateProductStock(item.getId(), item.getQuantity());
+		});
 
-    return invoice;
-  }
+		return invoice;
+	}
 
-  @Override
-  public Invoice updateInvoice(Invoice invoice) {
-    Invoice invoiceDB = getInvoice(invoice.getId());
-    invoiceDB.setCustomerId(invoice.getCustomerId());
-    invoiceDB.setDescription(invoice.getDescription());
-    invoiceDB.setNumberInvoice(invoice.getNumberInvoice());
-    invoiceDB.getItems().clear();
-    invoiceDB.setItems(invoice.getItems());
-    return invoiceRepository.save(invoiceDB);
-  }
+	@Override
+	public Invoice updateInvoice(Invoice invoice) {
+		Invoice invoiceDB = getInvoice(invoice.getId());
+		invoiceDB.setCustomerId(invoice.getCustomerId());
+		invoiceDB.setDescription(invoice.getDescription());
+		invoiceDB.setNumberInvoice(invoice.getNumberInvoice());
+		invoiceDB.getItems().clear();
+		invoiceDB.setItems(invoice.getItems());
+		return invoiceRepository.save(invoiceDB);
+	}
 
-  @Override
-  public Invoice deleteInvoice(Long invoiceId) {
-    Invoice invoiceDB = getInvoice(invoiceId);
-    invoiceDB.setState("DELETED");
-    return invoiceRepository.save(invoiceDB);
-  }
+	@Override
+	public Invoice deleteInvoice(Long invoiceId) {
+		Invoice invoiceDB = getInvoice(invoiceId);
+		invoiceDB.setState("DELETED");
+		return invoiceRepository.save(invoiceDB);
+	}
 
-  @Override
-  public Invoice getInvoice(Long id) {
-    Invoice invoice = invoiceRepository.findById(id)
-        .orElseThrow(() -> new NotFoundException("Invoice NOT found for provided Id."));
+	@CircuitBreaker(name = "customer-service", fallbackMethod = "fallbackCustomerService")
+	@Override
+	public Invoice getInvoice(Long id) {
+		Invoice invoice = invoiceRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException("Invoice NOT found for provided Id."));
 
-    Customer customer = this.customerClient.getCustomer(invoice.getCustomerId()).getBody();
+		Customer customer = this.customerClient.getCustomer(invoice.getCustomerId()).getBody();
 
-    List<InvoiceItem> listInvoiceItems = invoice.getItems().stream().map(invoiceItem -> {
-      Product p = this.productClient.getProductById(invoiceItem.getProductId()).getBody();
-      invoiceItem.setProduct(p);
-      return invoiceItem;
-    }).collect(Collectors.toList());
+		List<InvoiceItem> listInvoiceItems = invoice.getItems().stream().map(invoiceItem -> {
+			Product p = this.productClient.getProductById(invoiceItem.getProductId()).getBody();
+			invoiceItem.setProduct(p);
+			return invoiceItem;
+		}).collect(Collectors.toList());
 
-    
-    invoice.setCustomer(customer);
-    invoice.setItems(listInvoiceItems);
-    return invoice;
-  }
+		invoice.setCustomer(customer);
+		invoice.setItems(listInvoiceItems);
+		return invoice;
+	}
 }
